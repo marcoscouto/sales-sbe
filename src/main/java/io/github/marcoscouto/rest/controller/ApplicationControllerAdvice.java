2aada3e48d56bcba2232b271aabfce168d6e388d
Marcos Couto <marcos_otuoc@hotmail.com>
2020-04-13 20:26:03 -0300

    Exception Handler

---
+++ src/main/java/io/github/marcoscouto/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package io.github.marcoscouto.rest.controller;
+
+import io.github.marcoscouto.exception.BusinessRuleException;
+import io.github.marcoscouto.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(BusinessRuleException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleBusinessRuleException(BusinessRuleException e){
+        return new ApiErrors(e.getMessage());
+    }
+
+}
