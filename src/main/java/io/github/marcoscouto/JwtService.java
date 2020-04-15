40c746e1d129209464bea4d99eda7320e697b66e
Marcos Couto <marcos_otuoc@hotmail.com>
2020-04-15 10:22:14 -0300

    Security: Decode Token

--- src/main/java/io/github/marcoscouto/JwtService.java
+++ src/main/java/io/github/marcoscouto/JwtService.java
@@ -1,6 +1,8 @@
 package io.github.marcoscouto;
 
 import io.github.marcoscouto.domain.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,15 +21,44 @@ public class JwtService {
     @Value("${security.jwt.key}")
     private String key;
 
-    public String generateToken(User user){
+    public String generateToken(User user) {
         long expiration = Long.valueOf(this.expiration);
         LocalDateTime dateTimeExpiration = LocalDateTime.now().plusMinutes(expiration);
         Date date = Date.from(dateTimeExpiration.atZone(ZoneId.systemDefault()).toInstant());
+
+//        Map claims = new HashMap();
+//        claims.put("email", "user@email.com");
+//        claims.put("infos", "abcd");
+
         return Jwts.builder()
                 .setSubject(user.getUsername())
                 .setExpiration(date)
+//                .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, key)
                 .compact();
+    }
+
+    private Claims getClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Claims claims = getClaims(token);
+            Date dateExpiration = claims.getExpiration();
+            LocalDateTime date = dateExpiration.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return LocalDateTime.now().isAfter(date);
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    public String getUsernameUser(String token) throws ExpiredJwtException{
+        return (String) getClaims(token).getSubject();
     }
 
 }
